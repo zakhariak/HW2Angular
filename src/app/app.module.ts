713fd9bf9d580a0e0c3b1b14cf1802c02fe791ec
zakhariak <azakhariak@gmail.com>
2020-07-08 19:07:50 +0300

    first commit HW1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './homework1/header/header.component';
+import { MainComponent } from './homework1/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
+  exports: [],
+  entryComponents: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
